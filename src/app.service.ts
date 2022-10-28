2f0094256d0e297ab0f879467d6533491af0ee1b
euxzy <rijal.m12l@gmail.com>
2022-10-28 21:08:37 +0700

    create users route

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,17 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getUsers(): Array<object> {
+    return [
+      {
+        name: 'Muhamad Rijal',
+        domisili: 'Kab. Bandung Barat',
+      },
+      {
+        name: 'Test User',
+        domisili: 'Kota Bandung',
+      },
+    ];
+  }
 }
